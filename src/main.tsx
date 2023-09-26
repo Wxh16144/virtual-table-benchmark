25e43e26c601b3b96c275286d08ff3402d1b00f8
wuxh <wxh1220@gmail.com>
2023-09-26 16:53:04 +0800

    chore: 初始化项目

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+import 'normalize.css';
+import { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
+
+// @ts-ignore
+import routes from '~react-pages';
+
+window.console.log('%cPlayground => main.tsx', 'color:#0B7EA4;', {
+  routes,
+});
+
+const App = () => {
+  return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>;
+};
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Router>
+    <App />
+  </Router>
+);
